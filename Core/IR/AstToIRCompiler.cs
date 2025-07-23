08f9ba1a83c8806e09c7b04312ba567e6ad1b626
Cppshizoid <alex.krivoschecov@gmail.com>
2025-07-23 18:09:03 +0700

    first iteration of IR

---
+++ Core/IR/AstToIRCompiler.cs
@@ -0,0 +1,145 @@
+﻿using VM.Parser;
+using VM.Core.IR.Nodes;
+
+namespace VM.Core.IR
+{
+    public class AstToIrCompiler : IAstVisitor<List<IRNode>>
+    {
+        public List<IRNode> Visit(ProgramNode node)
+        {
+            var list = new List<IRNode>();
+            foreach (var stmt in node.Statements)
+                list.AddRange(stmt.Accept(this));
+            return list;
+        }
+
+        public List<IRNode> Visit(PrintStmt node)
+        {
+            var expr = node.Expressions.Count > 0 ? CompileExpr(node.Expressions[0]) : new IRConst { Value = "", Type = "string" };
+            return [ new IRPrint { Expr = expr, Line = node.Line } ];
+        }
+
+        public List<IRNode> Visit(LetStmt node)
+        {
+            return [ new IRLet
+            {
+                Name = node.Id!,
+                Expr = CompileExpr(node.Expression!),
+                Line = node.Line
+            } ];
+        }
+
+        public List<IRNode> Visit(InputStmt node)
+        {
+            return [ new IRInput
+            {
+                VarNames = [..node.Ids],
+                Line = node.Line
+            } ];
+        }
+
+        public List<IRNode> Visit(IfStmt node)
+        {
+            var irIf = new IRIf
+            {
+                Condition = CompileExpr(node.Condition),
+                ThenBlock = CompileBlock(node.ThenBranch),
+                ElseBlock = node.ElseBranch.Count > 0 ? CompileBlock(node.ElseBranch) : null,
+                Line = node.Line
+            };
+            return [ irIf ];
+        }
+
+        public List<IRNode> Visit(WhileStmt node)
+        {
+            return [ new IRWhile
+            {
+                Condition = CompileExpr(node.Condition!),
+                Body = CompileBlock(node.Body),
+                Line = node.Line
+            } ];
+        }
+
+        public List<IRNode> Visit(RepeatStmt node)
+        {
+            return [ new IRRepeat
+            {
+                Body = CompileBlock(node.Body),
+                Condition = CompileExpr(node.Condition!),
+                Line = node.Line
+            } ];
+        }
+
+        public List<IRNode> Visit(ForStmt node)
+        {
+            return [ new IRFor
+            {
+                VarName = node.Variable,
+                From = CompileExpr(node.From),
+                To = CompileExpr(node.To),
+                Step = node.Step != null ? CompileExpr(node.Step) : null,
+                Body = CompileBlock(node.Body),
+                Line = node.Line
+            } ];
+        }
+
+        public List<IRNode> Visit(ContinueStmt node)
+        {
+            return [ new IRGoto { Label = "__continue__", Line = node.Line } ];
+        }
+
+        public List<IRNode> Visit(ExitStmt node)
+        {
+            return [ new IRGoto { Label = "__break__", Line = node.Line } ];
+        }
+
+        public List<IRNode> Visit(BinaryExpr node)
+        {
+            throw new NotSupportedException("BinaryExpr должен обрабатываться как часть CompileExpr");
+        }
+
+        public List<IRNode> Visit(UnaryExpr node) => throw new NotSupportedException();
+        public List<IRNode> Visit(NumberExpr node) => throw new NotSupportedException();
+        public List<IRNode> Visit(StringExpr node) => throw new NotSupportedException();
+        public List<IRNode> Visit(VarExpr node) => throw new NotSupportedException();
+        public List<IRNode> Visit(FuncCallExpr node) => throw new NotSupportedException();
+
+        private List<IRNode> CompileBlock(List<StatementNode> stmts)
+        {
+            var list = new List<IRNode>();
+            foreach (var stmt in stmts)
+                list.AddRange(stmt.Accept(this));
+            return list;
+        }
+
+        private IRNode CompileExpr(ExprNode expr)
+        {
+            return expr switch
+            {
+                NumberExpr n => new IRConst { Value = n.Value, Type = "number", Line = n.Line },
+                StringExpr s => new IRConst { Value = s.Value, Type = "string", Line = s.Line },
+                VarExpr v => new IRVar { Name = v.Name, Line = v.Line },
+                BinaryExpr b => new IRBinary
+                {
+                    Op = b.Operator.ToString(),
+                    Left = CompileExpr(b.Left!),
+                    Right = CompileExpr(b.Right!),
+                    Line = b.Line
+                },
+                UnaryExpr u => new IRUnary
+                {
+                    Op = u.Operator.ToString(),
+                    Operand = CompileExpr(u.Operand!),
+                    Line = u.Line
+                },
+                FuncCallExpr f => new IRCall
+                {
+                    Name = f.Func.ToString().ToLower(),
+                    Args = f.Arguments.ConvertAll(CompileExpr),
+                    Line = f.Line
+                },
+                _ => throw new Exception($"Неизвестный тип выражения: {expr.GetType().Name}")
+            };
+        }
+    }
+}
---
+++ Core/IR/Nodes/IRNodes.cs
@@ -0,0 +1,127 @@
+﻿namespace VM.Core.IR.Nodes
+{
+    public abstract class IRNode
+    {
+        public int Line { get; init; }
+    }
+
+    public class IRConst : IRNode
+    {
+        public object Value;
+        public string Type; // "int", "float", "string", "bool"
+    }
+
+    public class IRVar : IRNode
+    {
+        public string Name;
+    }
+
+    public class IRLet : IRNode
+    {
+        public string Name;
+        public IRNode Expr;
+    }
+
+    public class IRPrint : IRNode
+    {
+        public IRNode Expr;
+    }
+
+    public class IRBinary : IRNode
+    {
+        public string Op;
+        public IRNode Left;
+        public IRNode Right;
+    }
+
+    public class IRUnary : IRNode
+    {
+        public string Op;
+        public IRNode Operand;
+    }
+
+    public class IRCall : IRNode
+    {
+        public string Name;
+        public List<IRNode> Args = [];
+    }
+
+    public class IRBlock : IRNode
+    {
+        public List<IRNode> Statements = [];
+    }
+
+    public class IRIf : IRNode
+    {
+        public IRNode Condition;
+        public List<IRNode> ThenBlock = [];
+        public List<IRNode>? ElseBlock;
+    }
+
+    public class IRWhile : IRNode
+    {
+        public IRNode Condition;
+        public List<IRNode> Body = [];
+    }
+
+    public class IRRepeat : IRNode
+    {
+        public List<IRNode> Body = [];
+        public IRNode Condition;
+    }
+
+    public class IRFor : IRNode
+    {
+        public string VarName;
+        public IRNode From;
+        public IRNode To;
+        public IRNode? Step;
+        public List<IRNode> Body = [];
+    }
+
+    public class IRInput : IRNode
+    {
+        public List<string> VarNames = [];
+    }
+
+    public class IRGoto : IRNode
+    {
+        public string Label;
+    }
+
+    public class IRLabel : IRNode
+    {
+        public string Name;
+    }
+
+    public class IRReturn : IRNode
+    {
+        public IRNode? Expr;
+    }
+
+    public class IRFunctionDecl : IRNode
+    {
+        public string Name;
+        public List<string> Args = [];
+        public List<IRNode> Body = [];
+        public string ReturnType = "any";
+    }
+
+    public class IRNewArray : IRNode
+    {
+        public IRNode Size;
+        public string ElementType;
+    }
+
+    public class IRStructInit : IRNode
+    {
+        public string TypeName;
+        public Dictionary<string, IRNode> Fields = new();
+    }
+
+    public class IRFieldAccess : IRNode
+    {
+        public IRNode Target;
+        public string FieldName;
+    }
+}
